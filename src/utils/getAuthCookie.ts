e8b2ff4290b01da6a8bb051cf70d4f3babe6f243
Williams Williams <williamscherechiwilliams@gmail.com>
2025-04-29 03:47:44 +0100

    Add logging to getAuthCookie function for debugging access token retrieval

--- src/utils/getAuthCookie.ts
+++ src/utils/getAuthCookie.ts
@@ -2,5 +2,6 @@ import { cookies } from "next/headers";
 
 export async function getAuthCookie() {
   const cookieStore = await cookies();
+  console.log("Getting auth cookie:", cookieStore.get("access_token"));
   return cookieStore.get("access_token")?.value;
 }
